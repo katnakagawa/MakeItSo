d64cc7c68c5c69033f502867db7d99f3d1d83269
katsuya nakagawa <katnakagawa.m@gmail.com>
2021-04-17 13:16:50 +0900

    Initial Commit

---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Katsuya Nakagawa on 2021/04/17.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
